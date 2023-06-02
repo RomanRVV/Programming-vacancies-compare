0ff1136c24b0f119aac31357b7801312c0a980c5
RomanRVV <rvachyov.roma@yandex.ru>
2023-06-02 17:12:24 +0300

    Cleaned up code from indexes and got rid of extra arguments

--- main.py
+++ main.py
@@ -1,84 +1,67 @@
-from fetch_records import programming_languages, fetch_records_sj, fetch_records_hh
+from fetch_records import PROGRAMMING_LANGUAGES, fetch_vacancies_sj, fetch_vacancies_hh
 from work_with_salary import predict_rub_salary_hh, predict_rub_salary_sj
 from terminaltables import AsciiTable
-import argparse
 from dotenv import load_dotenv, find_dotenv
 import os
 
 
-def sort_average_salary_hh_by_language(vacancies, language):
-    vacancies_number = {}
+def get_salary_statistics_hh(vacancies):
+    salary_statistics = {}
     salaries = []
     for vacancy in vacancies:
         for salary in vacancy['items']:
             average_salary = predict_rub_salary_hh(salary)
-            if not average_salary:
-                continue
-            else:
+            if average_salary:
                 salaries.append(average_salary)
-    total_average_salary = sum(salaries) / len(salaries)
-    vacancies_number[language] = {
-            "average_salary": int(total_average_salary),
-            "vacancies_processed": len(salaries),
+    try:
+        total_average_salary = sum(salaries) / len(salaries)
+        salary_statistics.update({
+                "average_salary": int(total_average_salary),
+                "vacancies_processed": len(salaries),
+                "vacancies_found": vacancy['found']
+            })
+    except ZeroDivisionError:
+        salary_statistics.update({
+            "average_salary": 'Нет результатов',
+            "vacancies_processed": 'Нет результатов',
             "vacancies_found": vacancy['found']
-        }
-    return vacancies_number
+         })
+    return salary_statistics
 
 
-def sort_average_salary_sj_by_language(vacancies, language):
-    vacancies_number = {}
+def get_salary_statistics_sj(vacancies):
+    salary_statistics = {}
     salaries = []
     for vacancy in vacancies:
         for salary in vacancy['objects']:
             average_salary = predict_rub_salary_sj(salary)
-            if not average_salary:
-                continue
-            else:
+            if average_salary:
                 salaries.append(average_salary)
     try:
         total_average_salary = sum(salaries) / len(salaries)
-        vacancies_number[language] = {
+        salary_statistics.update({
                 "average_salary": int(total_average_salary),
                 "vacancies_processed": len(salaries),
                 "vacancies_found": vacancy['total']
-            }
+            })
     except ZeroDivisionError:
-        vacancies_number[language] = {
+        salary_statistics.update({
                 "average_salary": 'Нет результатов',
                 "vacancies_processed": 'Нет результатов',
-                "vacancies_found": 'Нет результатов'
-            }
-    return vacancies_number
+                "vacancies_found": vacancy['total']
+            })
+    return salary_statistics
 
 
-def create_salary_table_sj(salary_statistics):
-    title = 'SuperJob Moscow'
+def create_salary_table(salary_statistics, title):
     table = [
         ['Язык программирования', ' Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
     ]
-    for count, salary in enumerate(salary_statistics):
-        language = [*salary.keys()]
-        table.append([*salary.keys(),
-                      salary_statistics[count][f'{language[0]}']['vacancies_found'],
-                      salary_statistics[count][f'{language[0]}']['vacancies_processed'],
-                      salary_statistics[count][f'{language[0]}']['average_salary']]
-                     )
-    table_instance = AsciiTable(table, title)
-    table_instance.justify_columns[2] = 'right'
-    return table_instance.table
-
-
-def create_salary_table_hh(salary_statistics):
-    title = 'HeadHunter Moscow'
-    table = [
-        ['Язык программирования', ' Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
-    ]
-    for count, salary in enumerate(salary_statistics):
-        language = [*salary.keys()]
-        table.append([*salary.keys(),
-                      salary_statistics[count][f'{language[0]}']['vacancies_found'],
-                      salary_statistics[count][f'{language[0]}']['vacancies_processed'],
-                      salary_statistics[count][f'{language[0]}']['average_salary']]
+    for language, salary in salary_statistics.items():
+        table.append([language,
+                      salary['vacancies_found'],
+                      salary['vacancies_processed'],
+                      salary['average_salary']]
                      )
     table_instance = AsciiTable(table, title)
     table_instance.justify_columns[2] = 'right'
@@ -88,25 +71,18 @@ def create_salary_table_hh(salary_statistics):
 def main():
     load_dotenv(find_dotenv())
     superjob_key = os.environ['SUPERJOB_KEY']
-    parser = argparse.ArgumentParser(
-        description='Скачивает и выводит зарплатную статистику вакансий с сайтов SuperJob и HH'
-    )
-    parser.add_argument('--add_language', help='Добавить язык программирования для сбора статистики')
-    args = parser.parse_args()
-    if args.add_language:
-        programming_languages.append(args.add_language)
-    vacancy_hh = {}
-    vacancy_sj = {}
-    vacancies_sj = []
-    vacancies_hh = []
-    for language in programming_languages:
-        vacancy_hh[language] = fetch_records_hh(language)
-        vacancy_sj[language] = fetch_records_sj(language, superjob_key)
-        vacancies_sj.append(sort_average_salary_sj_by_language(vacancy_sj[language], language))
-        vacancies_hh.append(sort_average_salary_hh_by_language(vacancy_hh[language], language))
+    hh_vacancies = {}
+    sj_vacancies = {}
+    sj_salary_statistics = {}
+    hh_salary_statistics = {}
+    for language in PROGRAMMING_LANGUAGES:
+        hh_vacancies[language] = fetch_vacancies_hh(language)
+        sj_vacancies[language] = fetch_vacancies_sj(language, superjob_key)
+        sj_salary_statistics[language] = get_salary_statistics_sj(sj_vacancies[language])
+        hh_salary_statistics[language] = get_salary_statistics_hh(hh_vacancies[language])
 
-    print(create_salary_table_sj(vacancies_sj))
-    print(create_salary_table_hh(vacancies_hh))
+    print(create_salary_table(sj_salary_statistics, 'SuperJob Moscow'))
+    print(create_salary_table(hh_salary_statistics, 'HeadHunter Moscow'))
 
 
 if __name__ == '__main__':
